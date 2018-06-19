fb41cf10ef52ed97d45f8f8f4b2355cc85b94658
surendra <surendra.dangol@daubajee.com>
2018-06-19 12:56:55 +0200

    Add class

---
+++ src/main/java/com/daubajee/dheba/peer/PeerMessage.java
@@ -0,0 +1,41 @@
+package com.daubajee.dheba.peer;
+
+import io.vertx.core.json.JsonObject;
+
+public class PeerMessage {
+
+    private final String type;
+
+    private final JsonObject content;
+
+    public PeerMessage(String type, JsonObject content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public JsonObject getContent() {
+        return content;
+    }
+
+
+    public boolean isValid() {
+        return !type.isEmpty() && !content.isEmpty();
+    }
+
+    public static PeerMessage from(JsonObject packet) {
+        String type = packet.getString(S.TYPE, "");
+        JsonObject content = packet.getJsonObject(S.CONTENT, new JsonObject());
+        return new PeerMessage(type, content);
+    }
+    
+    public JsonObject toJson() {
+        return new JsonObject()
+                    .put(S.TYPE, type)
+                    .put(S.CONTENT, content);
+    }
+
+}
