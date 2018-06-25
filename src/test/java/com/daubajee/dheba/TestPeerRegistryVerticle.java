780c5a1ea8938fecbe3eb06082df204421c05871
Surendra Dangol <suen.da@gmail.com>
2018-06-25 14:05:50 +0200

    Add testcase

--- src/test/java/com/daubajee/dheba/TestPeerRegistryVerticle.java
+++ src/test/java/com/daubajee/dheba/TestPeerRegistryVerticle.java
@@ -15,6 +15,7 @@ import com.daubajee.dheba.peer.PeerRegistryMessage;
 import com.daubajee.dheba.peer.PeerRegistryVerticle;
 import com.daubajee.dheba.peer.RemotePeerEvent;
 import com.daubajee.dheba.peer.S;
+import com.daubajee.dheba.peer.msg.GetPeerList;
 import com.daubajee.dheba.peer.msg.HandShake;
 import com.daubajee.dheba.peer.msg.PeerList;
 
@@ -28,6 +29,7 @@ import io.vertx.junit5.VertxTestContext;
 import io.vertx.rx.java.ObservableFuture;
 import io.vertx.rx.java.ObservableHandler;
 import io.vertx.rx.java.RxHelper;
+import rx.Observable;
 
 @ExtendWith(VertxExtension.class)
 public class TestPeerRegistryVerticle {
@@ -38,7 +40,7 @@ public class TestPeerRegistryVerticle {
     @Test
     public void testRegistry(Vertx vertx, VertxTestContext testContext) throws Throwable {
 
-        Checkpoint checkpoint = testContext.checkpoint(4);
+        Checkpoint checkpoint = testContext.checkpoint(5);
 
         PeerRegistryVerticle peerRegistryVerticle = new PeerRegistryVerticle();
 
@@ -48,8 +50,10 @@ public class TestPeerRegistryVerticle {
 
         ObservableFuture<Message<JsonObject>> peerList1Stream = RxHelper.observableFuture();
         ObservableFuture<Message<JsonObject>> peerList2Stream = RxHelper.observableFuture();
-
+        ObservableFuture<Message<JsonObject>> peerList3Stream = RxHelper.observableFuture();
+        
         remotePeerEventStream
+        	.take(1)
             .map(msg -> msg.body())
             .filter(json -> json.getString(S.TYPE, "").equals(RemotePeerEvent.NEW_PEER))
             .subscribe(json -> {
@@ -58,7 +62,7 @@ public class TestPeerRegistryVerticle {
                 assertThat("a different port was expected", event.getRemoteHostPort(), equalTo(42042));
                 checkpoint.flag();
 
-                RemotePeerEvent connectedEvent = new RemotePeerEvent("127.0.0.1", 42042, RemotePeerEvent.HANDSHAKED, new HandShake().toJson());
+                RemotePeerEvent connectedEvent = new RemotePeerEvent("127.0.0.1", 42042, RemotePeerEvent.HANDSHAKED, handshakeTemplate().toJson());
                 eventBus.publish(Topic.REMOTE_PEER_EVENTS, connectedEvent.toJson());
                 
                 PeerRegistryMessage registryGetListMsg = new PeerRegistryMessage(PeerRegistryMessage.GET_LIST, new JsonObject());
@@ -103,8 +107,35 @@ public class TestPeerRegistryVerticle {
                         assertThat(peerAdrStr + " =? " + peersAdrPort, peersAdrPort.contains(peerAdrStr), is(true));
                     });
                 checkpoint.flag();
+                
+
+                String incomingIp = "187.0.0.1";
+                RemotePeerEvent connectedEvent = new RemotePeerEvent(incomingIp, 45211, RemotePeerEvent.CONNECTED);
+        		eventBus.publish(Topic.REMOTE_PEER_EVENTS, connectedEvent.toJson());
+
+        		HandShake handShake = handshakeTemplate();
+        		handShake.setAddrMe(incomingIp+":45211");
+        		RemotePeerEvent handshakedEvent = new RemotePeerEvent(incomingIp, 45211, RemotePeerEvent.HANDSHAKED, handShake.toJson());
+        		eventBus.publish(Topic.REMOTE_PEER_EVENTS, handshakedEvent.toJson());
+        		GetPeerList getListRequest = new GetPeerList(1, peersAdrPort);
+                PeerRegistryMessage registryGetListMsg = new PeerRegistryMessage(PeerRegistryMessage.GET_LIST, getListRequest.toJson());
+                eventBus.send(Topic.PEER_REGISTRY, registryGetListMsg.toJson(), peerList3Stream.toHandler());
             });
 
+        peerList3Stream
+        	.take(1)
+            .map(msg -> msg.body())
+            .filter(json -> json.getString(S.TYPE, "").equals(PeerRegistryMessage.LIST))
+            .subscribe(json -> {
+                PeerRegistryMessage msg = PeerRegistryMessage.from(json);
+                JsonObject content = msg.getContent();
+                List<String> peers = PeerList.from(content).getPeers();
+                assertThat(peers.size(), equalTo(1));
+                String peerAdrPort = peers.get(0);
+                assertThat(peerAdrPort, equalTo("187.0.0.1:45211"));
+                checkpoint.flag();
+            });
+        	
         eventBus.consumer(Topic.REMOTE_PEER_EVENTS, remotePeerEventStream.toHandler());
 
         System.setProperty(Config.P_P2P_SEEDS, "127.0.0.1:42042");
@@ -112,6 +143,17 @@ public class TestPeerRegistryVerticle {
             checkpoint.flag();
         }));
 
-
     }
+
+	private static HandShake handshakeTemplate() {
+		HandShake handShake = new HandShake();
+        handShake.setAddrMe("localhost:42041");
+        handShake.setAddrYou("localhost:42042");
+        handShake.setAgent("Agent 1");
+        handShake.setServices("SERVICES BETA");
+        handShake.setVersion("0.1");
+        handShake.setTimestamp(System.currentTimeMillis());
+        handShake.setBestHeight(1);
+		return handShake;
+	}
 }
