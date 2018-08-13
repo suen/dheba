79fbdac9e35846ff4c98e70a160e5e490663f2ad
Surendra Dangol <suen.da@gmail.com>
2018-08-13 16:29:41 +0200

    Add class for constant values

---
+++ src/main/java/com/daubajee/dheba/block/BlockConstant.java
@@ -0,0 +1,9 @@
+package com.daubajee.dheba.block;
+
+public final class BlockConstant {
+
+    public static final int BLOCK_GENERATION_INTERVAL = 10;
+
+    public static final int DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
+
+}
