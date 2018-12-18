de233a87a1df5d8ebe15aae3ce847d7358c75c0a
Surendra Dangol <suen.da@gmail.com>
2018-12-18 22:28:05 +0100

    Add Chain C to the test.

--- src/test/java/com/daubajee/dheba/TestBlockchain.java
+++ src/test/java/com/daubajee/dheba/TestBlockchain.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -20,36 +21,86 @@ import io.reactivex.Observable;
 public class TestBlockchain {
 
     @Test
-    public void testAppendBlockTwoChains() {
+    public void testAppendBlockChains() {
         Blockchain chainA = new Blockchain();
         Blockchain chainB = new Blockchain();
+        Blockchain chainC = new Blockchain();
 
-        BlockHeader genesisHash = chainA.getLastHeader();
-        assertThat("Genesis hashes are not equal", genesisHash.getHash(),
+        BlockHeader genesisBlockHeader = chainA.getLastHeader();
+        assertThat("Genesis hashes are not equal", genesisBlockHeader.getHash(),
                 equalTo(chainB.getLastHeader().getHash()));
 
+        //Simulation of three chains.
+        
         long diff = 300000L;
         for (int i = 0; i < 15; i++) {
-            BlockHeader lastHeader = chainA.getLastHeader();
-            Block current = chainA.getBlock(lastHeader).get();
+            BlockHeader lastHeaderA = chainA.getLastHeader();
+            Block currentA = chainA.getBlock(lastHeaderA).get();
+
             Function<String, Block> blockLookupChainA = hash -> chainA.getBlock(hash).get();
-            long difficultyNextBlock = BlockUtils.getDifficultyForNextBlock(current, blockLookupChainA);
-            long ts = current.getTimestamp() + diff;
-            Block rawBlock = new Block(current.getIndex() + 1, "", current.getHash(), ts, 0,
+
+            long difficultyNextBlock = BlockUtils.getDifficultyForNextBlock(currentA, blockLookupChainA);
+            long ts = currentA.getTimestamp() + diff;
+            Block rawBlockA = new Block(currentA.getIndex() + 1, "", currentA.getHash(), ts, 0,
                     difficultyNextBlock, "Test Block");
-            Block nextBlock = BlockMiner.findNonce(rawBlock, 0, 1_000_000_000_000L)
+
+            Block nextBlockA = BlockMiner.findNonce(rawBlockA, 0, 1_000_000_000_000L)
                 .filter(block -> !block.getHash().isEmpty())
                 .blockingFirst();
-            Optional<BlockHeader> result = chainA.append(nextBlock);
+
+            Optional<BlockHeader> result = chainA.append(nextBlockA);
             assertThat("Block rejected", result.isPresent(), is(true));
-            System.out.println(Thread.currentThread().getName() + " - " + nextBlock.toJson());
-            BlockHeader acceptedBlockHeader = result.get();
-            Optional<Block> getAcceptedBlock = chainA.getBlock(acceptedBlockHeader);
 
-            assertThat("Block lookup failed", getAcceptedBlock.isPresent(), is(true));
+            System.out.println(Thread.currentThread().getName() + " - " + nextBlockA.toJson());
 
-            Optional<BlockHeader> blockBResult = chainB.append(nextBlock);
+            BlockHeader acceptedBlockHeaderA = result.get();
+            Optional<Block> getAcceptedBlockA = chainA.getBlock(acceptedBlockHeaderA);
+            assertThat("Block lookup failed", getAcceptedBlockA.isPresent(), is(true));
+
+            Optional<Block> nextBlockChainC;
+            if (i % 2 == 0) {
+                BlockHeader lastHeaderC = chainC.getLastHeader();
+
+                Block rawBlockC = new Block(lastHeaderC.getHeight() + 1, "", lastHeaderC.getHash(), ts, 0,
+                        difficultyNextBlock, "Test Block Chain C");
+
+                Block nextBlockC = BlockMiner.findNonce(rawBlockC, 0, 1_000_000_000_000L)
+                        .filter(block -> !block.getHash().isEmpty())
+                        .blockingFirst();
+
+                Optional<BlockHeader> resultAppendC = chainC.append(nextBlockC);
+                assertThat("Block rejected by Chain C", resultAppendC.isPresent(), is(true));
+
+                nextBlockChainC = Optional.of(nextBlockC);
+            } else {
+                nextBlockChainC = Optional.empty();
+            }
+
+            Optional<BlockHeader> resultAppendCA = chainC.append(nextBlockA);
+            assertThat("Chain C rejected BlockA", resultAppendCA.isPresent(), is(true));
+
+            Optional<BlockHeader> blockBResult = chainB.append(nextBlockA);
             assertThat("Chain B rejected block", blockBResult.isPresent(), is(true));
+
+            nextBlockChainC.ifPresent(blkC -> {
+                Optional<BlockHeader> resultAppendBC = chainB.append(blkC);
+                assertThat("C", resultAppendBC.isPresent(), is(true));
+            });
+
+        }
+
+        List<BlockHeader> headersA = chainA.getHeaders(genesisBlockHeader, 20);
+        List<BlockHeader> headersB = chainB.getHeaders(genesisBlockHeader, 20);
+        List<BlockHeader> headersC = chainC.getHeaders(genesisBlockHeader, 20);
+
+        for (int i = 0; i < headersA.size(); i++) {
+            BlockHeader headerA = headersA.get(i);
+            BlockHeader headerB = headersB.get(i);
+            BlockHeader headerC = headersC.get(i);
+            boolean aEqualsB = headerA.equals(headerB);
+            boolean aEqualsC = headerA.equals(headerC);
+            assertThat("Blocks at index " + i  + " on chain A and B not equal", aEqualsB, is(true));
+            assertThat("Blocks at index " + i  + " on chain A and C not equal", aEqualsC, is(true));
         }
 
     }
